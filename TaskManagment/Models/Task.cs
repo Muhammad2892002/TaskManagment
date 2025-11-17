ffaa4b6c7779bb956f30773871ed1cb67c15caba
muhammad Hadidi <muhammadhadidi1221@gmail.com>
2025-11-17 17:43:32 +0300

    Add project files.

---
+++ TaskManagment/Models/Task.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManagment.Models
+{
+    public class Task
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string ? Description { get; set; }
+
+
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        [ForeignKey("Status")]
+        public long StatusId { get; set; }
+
+        public Lookup Status { get; set; }
+
+        [ForeignKey("user")]
+        public long UserId { get; set; }
+        public User user { get; set; }
+    }
+}
---
+++ TaskManagment/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskManagment.Models
+{
+    public class User
+    {
+     public long Id { get; set; }
+      public string Username { get; set; }
+      public string Email { get; set; }
+        public string HashedPassword { get; set; }
+
+
+
+
+    }
+}
